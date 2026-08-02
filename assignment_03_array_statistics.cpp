024fe1eb648f2f776c14e08ef89bfbdcb3311ba4
obourbenedict1-crypto <obourbenedict1@gmail.com>
2026-08-02 05:55:22 -0700

    array statistics done

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,3 +42,62 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
+int getSum(int arr[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+double getAverage(int arr[], int n) {
+    return static_cast<double>(getSum(arr, n)) / n;
+}
+
+int getMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int getMin(int arr[], int n) {
+    int min = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+int main() {
+    int n;
+    cout << "How many numbers? ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Error: The number of values must be a positive integer." << endl;
+        return 0;
+    }
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cout << "Enter number " << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+
+    cout << "\nResults:" << endl;
+    cout << "Sum:     " << getSum(arr, n) << endl;
+    cout << "Average: " << getAverage(arr, n) << endl;
+    cout << "Maximum: " << getMax(arr, n) << endl;
+    cout << "Minimum: " << getMin(arr, n) << endl;
+
+    return 0;
+}
