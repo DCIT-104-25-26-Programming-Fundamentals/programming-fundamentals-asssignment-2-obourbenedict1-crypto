bfee6ceb5a564b71192252b80acdf525f2aae285
obourbenedict1-crypto <obourbenedict1@gmail.com>
2026-08-02 05:59:53 -0700

    fibonacci sequence done

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -51,3 +51,63 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
+void printFibonacci(int n) {
+    if (n <= 0) {
+        cout << "Error: Number of terms must be a positive integer." << endl;
+        return;
+    }
+
+    int a = 0, b = 1;
+
+    cout << "Fibonacci sequence: ";
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    cout << endl;
+}
+
+bool isFibonacci(int num) {
+    if (num < 0) {
+        return false;
+    }
+
+    int a = 0, b = 1;
+
+    while (a <= num) {
+        if (a == num) {
+            return true;
+        }
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return false;
+}
+
+int main() {
+    int n;
+    cout << "How many terms? ";
+    cin >> n;
+    printFibonacci(n);
+
+    cout << endl;
+
+    int num;
+    cout << "Enter a number to check: ";
+    cin >> num;
+
+    if (isFibonacci(num)) {
+        cout << num << " is a Fibonacci number." << endl;
+    } else {
+        cout << num << " is NOT a Fibonacci number." << endl;
+    }
+
+    return 0;
+}
